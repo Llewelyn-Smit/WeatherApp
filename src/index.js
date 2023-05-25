6ec9ddad1b4b6c9df36740202fb3ef816a15818e
Karel <karel.nle21@gmail.com>
2023-05-25 20:54:02 +0200

    express-server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import Headpartial from './headFunction';
@@ -15,3 +15,35 @@ const searching = ReactDOM.createRoot(searcher);
 searching.render(<Searchpartial/>);
 
 reportWebVitals();
+
+class App extends Component {
+state = {
+    data: null
+  };
+
+  componentDidMount() {
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }
+    // fetching the GET route from the Express server which matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+    return body;
+  };
+
+  render() {
+    return (
+      <div>
+        <p className="App-intro">{this.state.data}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
