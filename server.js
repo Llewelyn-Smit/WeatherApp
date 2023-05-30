898e4c1e053475f4291956f0da20dfb18103e675
Llewelyn-Smit <111749335+Llewelyn-Smit@users.noreply.github.com>
2023-05-30 20:54:50 +0200

    not working

--- server.js
+++ server.js
@@ -1,11 +1,117 @@
 const express = require('express'); //Line 1
+
+const moment = require('moment');
+
+ 
+
 const app = express(); //Line 2
+
 const port = process.env.PORT || 5000; //Line 3
 
+
+
+
+
+app.get('/', (req, res) => {
+
+  res.send('Server is running.'); // Or redirect to another route
+
+});
+
+ 
+
+// dont with it it works
+
+app.get('/express_backend', async (req, res) => {
+
+  try {
+
+    const currentDate = moment();
+
+    let data = [];
+
+    let dateRange = [];
+
+ 
+
+    for (let i = -3; i <= 1; i++) {
+
+      const formattedDate = currentDate.clone().add(i, 'days').format('YYYY-MM-DD');
+console.log(formattedDate);
+      dateRange.push(formattedDate);
+
+    }
+
+    for (let date of dateRange) {
+
+      let response = await fetch(`https://api.weatherapi.com/v1/history.json?key=e1181995b6bc491dbd6182017232305&q=New York&dt=${date}`);
+
+      let  forecast  = await response.json();
+
+
+      data.push(forecast);
+
+    }
+    let returnData = [];
+    let fdays= [];
+    data.forEach(element => {
+      fdays.push(element.forecast.forecastday)
+   
+    });
+   
+  let normDays=[];  
+    
+fdays.forEach(element => {
+   normDays.push(element)
+});
+
+
+
+var keyValuePairs = [];
+
+fdays.forEach(function(innerArray) {
+  innerArray.forEach(function(obj) {
+    Object.entries(obj).forEach(function([key, value]) {
+      if (key === "day" || key === "date") {
+      keyValuePairs.push({ key: key, value: value });
+      }
+    });
+  });
+});
+
+for (let i = 0; i < keyValuePairs.length; i++) {
+  let obj = {
+    date : keyValuePairs[i].value,
+    temp : keyValuePairs[i+1].value.avgtemp_c,
+     condition: keyValuePairs[i+1].value.condition.text,
+    picture:keyValuePairs[i+1].value.condition.icon}
+ i++;
+  returnData.push(obj)
+   }
+
+
+    //console.log(data);
+    console.log('about to send return data')
+console.log(returnData);
+res.set('Content-Type', 'application/json');
+    res.json(returnData);
+
+  } catch (error) {
+
+    console.error(error);
+
+    res.status(500).send({ error: 'An error occurred' });
+
+  }
+
+});
+
+
+
+
+
 // This displays message that the server running and listening to specified port
+
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 
-// create a GET route
-app.get('/express_backend', (req, res) => { //Line 9
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
-}); //Line 11
+
--- src/index.js
+++ src/index.js
@@ -1,49 +1,76 @@
-import React, { Component } from 'react';
+import React from 'react';
+//import {createRoot} from 'react-dom/client';
 import ReactDOM from 'react-dom/client';
+import { Component } from 'react';
+
 import reportWebVitals from './reportWebVitals';
 import Headpartial from './headFunction';
 import Searchpartial from './searchFunction';
 import Weekpartial from './weekFunction';
+
+
+
+
+
 const header = document.getElementById('top');
 const head = ReactDOM.createRoot(header);
-head.render(<Headpartial/>);
+head.render(<Headpartial />);
+
 const weekly = document.getElementById('week');
 const week = ReactDOM.createRoot(weekly);
-week.render(<Weekpartial/>);
+week.render(<Weekpartial />);
+
 const searcher = document.getElementById('search');
 const searching = ReactDOM.createRoot(searcher);
-searching.render(<Searchpartial/>);
+searching.render(<Searchpartial />);
 
 reportWebVitals();
 
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
 class App extends Component {
-state = {
+  state = {
     data: null
   };
 
   componentDidMount() {
     this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
+      .then(res => this.setState({ data: res }))
       .catch(err => console.log(err));
   }
-    // fetching the GET route from the Express server which matches the GET route from server.js
+
   callBackendAPI = async () => {
     const response = await fetch('/express_backend');
     const body = await response.json();
 
     if (response.status !== 200) {
-      throw Error(body.message) 
+      throw Error(body.message);
     }
+
     return body;
   };
 
   render() {
+    const { data } = this.state;
+
     return (
       <div>
-        <p className="App-intro">{this.state.data}</p>
+        {data && (
+          <ul>
+            {data.map(item => (
+              <li key={item.date}>
+                <p>Date: {item.date}</p>
+                <p>Temperature: {item.temp}</p>
+                <p>Condition: {item.condition}</p>
+                <img src={item.picture} alt="Weather Icon" />
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     );
   }
 }
 
-export default App;
+root.render(<App />/*,document.getElementById('root')*/);
