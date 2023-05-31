3072b93fde09a8b753753e759d7dcac7419eb7b9
Llewelyn-Smit <111749335+Llewelyn-Smit@users.noreply.github.com>
2023-05-31 10:27:14 +0200

    updating day by city

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import Headpartial from './headFunction';
 import SearchPartial from './searchFunction';
-import Weekpartial from './weekFunction';
+//import Weekpartial from './weekFunction';
 const header = document.getElementById('top');
 const head = ReactDOM.createRoot(header);
 head.render(<Headpartial/>);
-const weekly = document.getElementById('week');
+/*const weekly = document.getElementById('week');
 const week = ReactDOM.createRoot(weekly);
-week.render(<Weekpartial/>);
+week.render(<Weekpartial/>);*/
 const searcher = document.getElementById('search');
 const searching = ReactDOM.createRoot(searcher);
 searching.render(<SearchPartial/>);
