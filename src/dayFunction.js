401d3b74a5611867235193632538b7bcc0bba6a3
Llewelyn-Smit <111749335+Llewelyn-Smit@users.noreply.github.com>
2023-06-01 19:04:52 +0200

    Minimum Viable Product Done

--- src/dayFunction.js
+++ src/dayFunction.js
@@ -1,61 +1,92 @@
 import React from 'react';
-var timePart;
+
 function weekPartial(weatherResult, location, standard) {
   return (
-    <div className="container mt-2 bg-none">
-    <div className="card weekDisplayContainer">
-      <h5 className="card-title text-center mt-4">{location}</h5>
+<div className="container mt-2 bg-none">
+  <div className="card weekDisplayContainer">
+    <h5 className="card-title text-center mt-4">{location}</h5>
 
-      <div className="card-body weekDisplayContainer">
-        <div className="row justify-content-center">
-          <div className="col-lg-2 align-middle scollButton">
-            <div className="container">
-              {weatherResult.length > 0 && (
-                <div className="row">
-                  {weatherResult.map((hour, index) => (
-                    <div
-                      key={hour.time}
-                      className="col-lg-2 d-flex cont-border text-center"
-                    >
-                      <div className="">
-                        <div className="d-flex justify-content-center weather-box text-center">
-                          {split(hour.time)}
+    <div className="card-body weekDisplayContainer">
+      <div className="row justify-content-center">
+        {weatherResult.length > 0 && (
+          <div className="container">
+            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
+              {weatherResult.map((hour, index) => {
+                // Calculate the temperature value to be used for color gradient
+                let temperature = standard === 0 ? hour.temp_c : hour.temp_f;
+                temperature = parseFloat(temperature);
 
-                          <h6>{timePart}</h6>
+                // Calculate the color values based on the temperature
+                let gradientColor = getGradientColor(temperature,standard);
 
-                          <img
-                            src={hour.condition.icon}
-                            alt={hour.condition.text}
-                          />
-
-                          {console.log("tempStandard state:", standard)}
-
-                          {standard === 0 ? (
-                            <p>{hour.temp_c}</p>
-                          ) : (
-                            <p>{hour.temp_f}</p>
-                          )}
-                        </div>
+                return (
+                  <div
+                    key={hour.time}
+                    className="col d-flex cont-border align-items-center justify-content-center text-center p-3 rounded"
+                    style={{
+                      border: '1px solid #a9c4dd',
+                      background: `radial-gradient(circle at center, ${gradientColor}, #ffffff)`,
+                    }}
+                  >
+                    <div className="d-flex flex-column align-items-center justify-content-center">
+                      <div className="row align-items-center justify-content-center mb-2">
+                        <h6>{split(hour.time)}</h6>
+                      </div>
+                      <div className="row align-items-center justify-content-center mb-2">
+                        <img
+                          src={hour.condition.icon}
+                          alt={hour.condition.text}
+                          style={{ width: '50px', height: '50px' }}
+                        />
+                      </div>
+                      <div className="row align-items-center justify-content-center">
+                        {standard === 0 ? (
+                          <p>{hour.temp_c}</p>
+                        ) : (
+                          <p>{hour.temp_f}</p>
+                        )}
                       </div>
                     </div>
-                  ))}
-
-                  {Array.from(
-                    { length: Math.ceil(weatherResult.length / 6) },
-                    (_, i) => (
-                      <div key={i} className="w-100"></div>
-                    )
-                  )}
-                </div>
-              )}
+                  </div>
+                );
+              })}
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   </div>
+</div>
+
+
+
   );
 }
+function getGradientColor(temperature, standard) {
+  let minTemperature = -10; // Minimum temperature value (Celsius)
+  let maxTemperature = 40; // Maximum temperature value (Celsius)
+  // Define the color gradient range
+  const startColor = [0, 128, 255]; // RGB values for the starting color (blue)
+  const endColor = [255, 0, 0]; // RGB values for the ending color (red)
+
+  
+    let normalizedTemperature = temperature;
+  if (standard === 1) 
+  {
+normalizedTemperature = (temperature - 32) * (5 / 9);
+  }
+  // Calculate the percentage of the temperature within the range
+  const percentage = (normalizedTemperature - minTemperature) / (maxTemperature - minTemperature);
+
+  // Interpolate the RGB values based on the percentage
+  const interpolatedColor = startColor.map((start, index) =>
+    Math.round(start + percentage * (endColor[index] - start))
+  );
+
+  // Return the interpolated color as a CSS RGB string
+  return `rgb(${interpolatedColor.join(',')})`;
+}
+
 
 function split(a) {
   var dateTimeParts = a.split(" ");
--- src/headFunction.js
+++ src/headFunction.js
@@ -2,22 +2,52 @@ import React from 'react';
 
 function headPartial() {
   return (
-    <div className="container headPartial py-5">
+
+<div className="container">
+  <div className="row">
+    <div className="col d-flex justify-content-end">
+      <img className="img-thumbnail rounded-circle" style={{ width: '100px', height: '100px' }} src="/images/turtleLogo.jpg" alt="Turtle Logo" />
+    </div>
+    <div className="col">
       <div className="row">
-        <div className="col-lg-2 d-flex align-items-center">
-          <img className="img-thumbnail rounded-circle" style={{ width: '100px', height: '100px' }} src="/images/turtleLogo.jpg" alt="Turtle Logo" />
-        </div>
-        <div className="col-lg-10">
-          <div className="row d-flex" style={{ height: '100%' }}>
-            <div className="col-12 d-flex">
-              <h1 className="text-center display-2 font-weight-lighter">Turtleneck</h1>
-              <h3 className="text-center display-8 font-weight-light">Weather</h3>
-              <h3 className='text-center display-8 font-weight-light'> App</h3>
-            </div>
+        <h1 className="text-center font-weight-bold" style={{ fontSize: 'min(8vw, max(6vw, 40px))' }}>Turtleneck</h1>
+      </div>
+      <div className="row">
+        <h3 className="text-center font-weight-light" style={{ fontSize: 'min(4vw, 2.5vw)' }}>WeatherApp</h3>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+/*
+
+<div className="container headPartial py-5">
+  <div className="row">
+    <div className="col-lg-10">
+      <div className="row d-flex" style={{ height: '100%' }}>
+        <div className="col-12 d-flex flex-column align-items-center">
+          <h1 className="text-center display-2 font-weight-lighter">Turtleneck</h1>
+          <div className="d-flex justify-content-center">
+            <h3 className="text-center display-8 font-weight-light">Weather</h3>
+            <h3 className='text-center display-8 font-weight-light'> App</h3>
           </div>
         </div>
       </div>
     </div>
+    <div className="col-lg-2 d-flex align-items-center">
+      <img className="img-thumbnail rounded-circle" style={{ width: '100px', height: '100px' }} src="/images/turtleLogo.jpg" alt="Turtle Logo" />
+    </div>
+  </div>
+</div>
+
+
+*/ 
+
+
+
+
   );
 }
 
