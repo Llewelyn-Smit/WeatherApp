3059a8ee3b0cbd3de01a18def4a18af523039cb9
Karel <karel.nle21@gmail.com>
2023-06-04 19:10:06 +0200

    styling

--- src/headFunction.js
+++ src/headFunction.js
@@ -3,12 +3,12 @@ import React from 'react';
 function headPartial() {
   return (
 
-<div className="container">
+<div className="container pt-3">
   <div className="row">
-    <div className="col-2 d-flex justify-content-center my-auto">
+    <div className="col-lg-2 col-5 d-flex justify-content-center my-auto">
       <img className="img-thumbnail rounded-circle" style={{ width: '100px', height: '100px' }} src="/images/turtleLogo.jpg" alt="Turtle Logo" />
     </div>
-    <div className="col-10">
+    <div className="col-lg-10 col-7">
       <div className="row">
         <h1 className="text-center" style={{ fontSize: 'min(8vw, max(6vw, 40px))' }}>Turtleneck</h1>
       </div>
